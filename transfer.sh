a2df869f5f00be1d76f8d90b0ebf067e01ca7690
Jason Hawk Harris <jasonhawkharris@gmail.com>
2022-07-12 12:52:47 -0500

    can now produce json file out of md file

--- transfer.sh
+++ transfer.sh
@@ -1,37 +1,64 @@
 #! /bin/bash
 
 mkdir ./resolved-tickets/converted
+mkdir ./resolved_tickets/converted/json
 touch ./resolved-tickets/converted/keyval.txt
-KEYVALS=./resolved-tickets/converted/keyval.txt
 
 ORIG=./resolved-tickets
 CONV=./resolved-tickets/converted
 TICKET="2935"
+KEYS=()
+VALS=()
 
-# delete empty lines
-# grep "\S" "$ORIG/$TICKET.md"
+# ANCHOR create KEYS
+# function: create_key()
+# description: Creates correctly formatted JSON keys out of plain text
+# params: current format, new format, line number, char range
+create_key() {
+    gsed -i "s/-   $1/$2/" "$CONV/$TICKET.txt"
+    awk "NR==$3" "$CONV/$TICKET.txt" >"$CONV/$2.txt"
+    KEY=$(cut -c "$4" "$CONV/$2.txt")
+    KEYS+=("\"$KEY\"")
+}
+
+# ANCHOR create values
+# function create_value()
+# description: uses previously created files generate valid JSON values
+# params: filename, char range, type, append line
+create_value() {
+    VALUE=$(cut -c "$2" "$CONV/$1.txt")
+    # FIXME add condition to check type param for number, string, or array
+    if [ "$VALUE" = "" ]; then
+        VALUE="N/A"
+    fi
+    rm -rf "$CONV/$1.txt"
+    VALS+=("\"$VALUE\"")
+}
 
 pandoc -o "$CONV/$TICKET.txt" "$ORIG/$TICKET.md" --strip-comments --wrap=none
 
 # ANCHOR Prepare key-value pairs that need special treatment
-# FORMAT ticket number key:value
+# format ticket number key:value
 awk 'NR==3' "$CONV/$TICKET.txt" >"$CONV/ticketnumber.txt"
 TICKET_NUMBER=$(cut -c 21-24 "$CONV/ticketnumber.txt")
+KEYS+=("\"zendeskTicketNumber\"")
+VALS+=("$TICKET_NUMBER")
 rm "$CONV/ticketnumber.txt"
-echo "\"zendeskTicketNumber\": $TICKET_NUMBER," >>$KEYVALS
 
-#FORMAT ticket link key:value
+# format ticket link key:value
 awk 'NR==3' "$CONV/$TICKET.txt" >"$CONV/ticketlink.txt"
 TICKET_LINK=$(cut -c 27-76 "$CONV/ticketlink.txt")
+KEYS+=("\"zendeskLink\"")
+VALS+=("\"$TICKET_LINK\"")
 rm "$CONV/ticketlink.txt"
-echo "\"zendeskLink\": \"$TICKET_LINK\"," >>$KEYVALS
 
-# FORMAT title key:value
+# format title key:value
 # FIXME need to remove the pandoc artifact at the end of the title
 awk 'NR==1' "$CONV/$TICKET.txt" >"$CONV/title.txt"
 TITLE=$(cut -c 3- "$CONV/title.txt")
+KEYS+=("\"title\"")
+VALS+=("\"$TITLE\"")
 rm "$CONV/title.txt"
-echo "\"title\": \"$TITLE\"," >>$KEYVALS
 
 # ANCHOR Prepare the rest of the document for conversion to json key-value pairs
 # remove lines '#'
@@ -45,118 +72,47 @@ grep "\S" "$CONV/$TICKET-m.txt" >"$CONV/$TICKET.txt"
 # remove tmp txt files
 rm "$CONV/$TICKET.md" "$CONV/$TICKET-e.txt" "$CONV/$TICKET-m.txt"
 
-# replace doc values with key values corresponding to db
-# keeping this line for now but probably not needed
-gsed -i 's/-   Zendesk Link/"zendeskLink"/' "$CONV/$TICKET.txt"
+# ANCHOR create rest of keys
+create_key "Application engineer" "AER" 2 "1-3"
+create_key "Customer" "customer" 3 "1-8"
+create_key "Date" "dateClosed" 4 "1-10"
+create_key "Version" "version" 5 "1-7"
+create_key "Deployment" "deployment" 6 "1-10"
+create_key "External Services" "externalServices" 7 "1-16"
+create_key "Auth Providers" "authProviders" 8 "1-13"
+create_key "Slack Links" "slackLinks" 9 "1-10"
+create_key "GitHub Issue Link" "githubIssueLink" 10 "1-15"
+create_key "Doc Update Link" "docUpdateLink" 11 "1-13"
+KEYS+=("\"summary\"")
 
-# function create_key()
-# params: (current format, new format, line number, file name)
+# ANCHOR create rest of vals
+create_value "AER" "6-" "string"
+create_value "customer" "11-" "string"
+create_value "dateClosed" "13-" "string"
+create_value "version" "10-" "string"
+create_value "deployment" "13-" "string"
+create_value "externalServices" "19-" "array"
+create_value "authProviders" "16-" "array"
+create_value "slackLinks" "13-" "array"
+create_value "githubIssueLink" "18-" "string"
+create_value "docUpdateLink" "16-" "string"
+VALS+=("\"Sample summary for now\"")
 
-create_key() {
-    gsed -i "s/-   $1/$2/" "$CONV/$TICKET.txt"
-    awk "NR==$3" "$CONV/$TICKET.txt" >"$CONV/$2.txt"
-    KEY=$(cut -c "$4" "$CONV/$2.txt")
-    rm "$CONV/$2.txt"
-    echo "$KEY"
-}
+# Create key value pairs and push them to json file
+length=${#KEYS[@]}
+touch "$CONV/$TICKET.json"
 
-# key_args=( ("Application engineer" "AER" 2) ("Customer" "customer" 3) ("Date" "date" 4) )
-# echo "${key_args[$1]}"
-# ANCHOR create KEYS
-# "aer"
-create_key "Application engineer" "AER" 2 "1-"
-create_key "Customer" "customer" 3 "1-"
-create_key "Date" "dateClosed" 4 "1-"
-create_key "Version" "version" 5 "1-"
-create_key "Deployment" "deployment" 6 "1-"
-create_key "External Services" "externalServices" 7 "1-"
-create_key "Auth Providers" "authProviders" 8 "1-"
-create_key "Slack Links" "slackLinks" 9 "1-"
-create_key "GitHub Issue Link" "githubIssueLink" 10 "1-"
-create_key "Doc Update Link" "docUpdateLink" 11 "1-"
+for ((i = 0; i < length; i++)); do
+    if [ "$i" -eq 0 ]; then
+        echo "{" >>"$CONV/$TICKET.json"
+    fi
 
-# gsed -i 's/-   Application engineer/"AER"/' "$CONV/$TICKET.txt"
-# awk 'NR==2' "$CONV/$TICKET.txt" >"$CONV/aer.txt"
-# AER=$(cut -c 1-5 "$CONV/aer.txt")
-# rm "$CONV/aer.txt"
-# echo "$AER"
+    if [ "$i" -ne 13 ]; then
+        echo "${KEYS[$i]}: ${VALS[$i]}," >>"$CONV/$TICKET.json"
+    else
+        echo "${KEYS[$i]}: ${VALS[$i]}" >>"$CONV/$TICKET.json"
+        echo "}" >>"$CONV/$TICKET.json"
+    fi
+done
 
-# "customer"
-# gsed -i 's/-   Customer/"customer"/' "$CONV/$TICKET.txt"
-# awk 'NR==3' "$CONV/$TICKET.txt" >"$CONV/customer.txt"
-# customer=$(cut -c 1-10 "$CONV/customer.txt")
-# rm "$CONV/customer.txt"
-# echo "$customer"
-
-# # "dateClosed"
-# gsed -i 's/-   Date/"dateClosed"/' "$CONV/$TICKET.txt"
-# awk 'NR==4' "$CONV/$TICKET.txt" >"$CONV/date-closed.txt"
-# dateClosed=$(cut -c 1-12 "$CONV/date-closed.txt")
-# rm "$CONV/date-closed.txt"
-# echo "$dateClosed"
-
-# Auth Providers Slack Links Github Issue Link Doc Update Link
-
-# "version"
-# gsed -i 's/-   Version/"version"/' "$CONV/$TICKET.txt"
-# awk 'NR==5' "$CONV/$TICKET.txt" >"$CONV/version.txt"
-# version=$(cut -c 1-9 "$CONV/version.txt")
-# rm "$CONV/version.txt"
-# echo "$version"
-
-# # "deployment"
-# gsed -i 's/-   Deployment/"deployment"/' "$CONV/$TICKET.txt"
-# awk 'NR==6' "$CONV/$TICKET.txt" >"$CONV/deployment.txt"
-# deployment=$(cut -c 1-12 "$CONV/deployment.txt")
-# rm "$CONV/deployment.txt"
-# echo "$deployment"
-
-# # "externalServices"
-# gsed -i 's/-   External Services/"externalServices"/' "$CONV/$TICKET.txt"
-# awk 'NR==7' "$CONV/$TICKET.txt" >"$CONV/external-services.txt"
-# externalServices=$(cut -c 1-18 "$CONV/external-services.txt")
-# rm "$CONV/external-services.txt"
-# echo "$externalServices"
-
-# # "authProviders"
-# gsed -i 's/-   Auth Providers/"authProviders"/' "$CONV/$TICKET.txt"
-# awk 'NR==8' "$CONV/$TICKET.txt" >"$CONV/auth-providers.txt"
-# authProviders=$(cut -c 1-15 "$CONV/auth-providers.txt")
-# rm "$CONV/auth-providers.txt"
-# echo "$authProviders"
-
-# # "slackLinks"
-# gsed -i 's/-   Slack Links/"slackLinks"/' "$CONV/$TICKET.txt"
-# awk 'NR==9' "$CONV/$TICKET.txt" >"$CONV/slack-links.txt"
-# slackLinks=$(cut -c 1-12 "$CONV/slack-links.txt")
-# rm "$CONV/slack-links.txt"
-# echo "$slackLinks"
-
-# # "githubIssueLink"
-# gsed -i 's/-   GitHub Issue Link/"githubIssueLink"/' "$CONV/$TICKET.txt"
-# awk 'NR==10' "$CONV/$TICKET.txt" >"$CONV/github-issue-link.txt"
-# githubIssueLink=$(cut -c 1-17 "$CONV/github-issue-link.txt")
-# rm "$CONV/github-issue-link.txt"
-# echo "$githubIssueLink"
-
-# # "docUpdateLink"
-# gsed -i 's/-   Doc Update Link/"docUpdateLink"/' "$CONV/$TICKET.txt"
-# awk 'NR==11' "$CONV/$TICKET.txt" >"$CONV/doc-update-link.txt"
-# docUpdateLink=$(cut -c 1-15 "$CONV/doc-update-link.txt")
-# rm "$CONV/doc-update-link.txt"
-# echo "$docUpdateLink"
-
-# "summary"
-summary="\"summary\""
-echo "$summary"
-
-# rm -rf "$CONV"
-
-# extract ticket number and create zendeskTicketNumberField
-# echo '"zendeskTicketNumber": $TICKET,' "$CONV/$TICKET.txt"
-
-# CLEAN UP
-# echo "====================================="
-# cat "$CONV/$TICKET.txt"
-# rm -rf $CONV
-# cat "$CONV/$TICKET.txt"
+exit 0
