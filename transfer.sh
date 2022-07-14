f1f64eef8941941f9d20a3f5b44c5e336ff90f8b
Jason Hawk Harris <jasonhawkharris@gmail.com>
2022-07-14 13:32:19 -0500

    adds testing script.

--- transfer.sh
+++ transfer.sh
@@ -5,12 +5,16 @@ if [ ! -d ./converted-resolved-tickets ]; then
     mkdir ./converted-resolved-tickets
 fi
 
-if [ ! -d ./converted-resolved-tickets/json ]; then
-    mkdir ./converted-resolved-tickets/json
+if [ ! -d ./converted-resolved-tickets/key-vals ]; then
+    mkdir ./converted-resolved-tickets/key-vals
 fi
 
-if [ -f "./converted-resolved-tickets/$1.txt" ]; then
-    echo "File already exists at ./converted-resolved-tickets/$1.txt"
+if [ ! -d ./converted-resolved-tickets/text ]; then
+    mkdir ./converted-resolved-tickets/text
+fi
+
+if [ -f "./converted-resolved-tickets/text/$1.txt" ]; then
+    echo "File already exists at ./converted-resolved-tickets/text/$1.txt"
     exit 1
 fi
 
@@ -38,97 +42,102 @@ echo "============== TICKET $1 ==============="
 #     echo "create value function"
 # }
 
-pandoc -o "$CONV/$TICKET.txt" "$ORIG/$TICKET.md" --strip-comments --wrap=none
+pandoc -o "$CONV/text/$TICKET.txt" "$ORIG/$TICKET.md" --strip-comments --wrap=none
 
 # ANCHOR Prepare key-value pairs that need special treatment
 # format ticket number key:value
-awk 'NR==3' "$CONV/$TICKET.txt" >"$CONV/ticketnumber.txt"
+awk 'NR==3' "$CONV/text/$TICKET.txt" >"$CONV/ticketnumber.txt"
 CHECK="$(cut -c 5 "$CONV/ticketnumber.txt")"
 
 if [ ! "$CHECK" = "*" ]; then
     TICKET_NUMBER=$(cut -c 21-24 "$CONV/ticketnumber.txt")
-    echo "zendeskTicketNumber: $TICKET_NUMBER" >>"$CONV/$KV.txt"
+    echo "zendeskTicketNumber: $TICKET_NUMBER" >>"$CONV/key-vals/$KV.txt"
 else
     TICKET_NUMBER=$(cut -c 25-28 "$CONV/ticketnumber.txt")
-    echo "zendeskTicketNumber: $TICKET_NUMBER" >>"$CONV/$KV.txt"
+    echo "zendeskTicketNumber: $TICKET_NUMBER" >>"$CONV/key-vals/$KV.txt"
 fi
 
 rm "$CONV/ticketnumber.txt"
 
 # format ticket link key:value
-awk 'NR==3' "$CONV/$TICKET.txt" >"$CONV/ticketlink.txt"
+awk 'NR==3' "$CONV/text/$TICKET.txt" >"$CONV/ticketlink.txt"
 
 if [ ! "$CHECK" = "*" ]; then
     TICKET_LINK=$(cut -c 27-76 "$CONV/ticketlink.txt")
-    echo "zendeskLink: $TICKET_LINK" >>"$CONV/$KV.txt"
+    echo "zendeskLink: $TICKET_LINK" >>"$CONV/key-vals/$KV.txt"
 else
     TICKET_LINK=$(cut -c 31-80 "$CONV/ticketlink.txt")
-    echo "zendeskLink: $TICKET_LINK" >>"$CONV/$KV.txt"
+    echo "zendeskLink: $TICKET_LINK" >>"$CONV/key-vals/$KV.txt"
 fi
 
 rm "$CONV/ticketlink.txt"
 
 # format title key:value
-awk 'NR==1' "$CONV/$TICKET.txt" >"$CONV/title.txt"
+awk 'NR==1' "$CONV/text/$TICKET.txt" >"$CONV/title.txt"
 TITLE=$(cut -c 3- "$CONV/title.txt")
 
 if [ "${TITLE: -10}" = " \`\`{=html}" ]; then
     TITLE_EDIT=${TITLE%??????????}
-    echo "title: $TITLE_EDIT" >>"$CONV/$KV.txt"
+    echo "title: $TITLE_EDIT" >>"$CONV/key-vals/$KV.txt"
 else
-    echo "title: $TITLE" >>"$CONV/$KV.txt"
+    echo "title: $TITLE" >>"$CONV/key-vals/$KV.txt"
 fi
 
 rm "$CONV/title.txt"
 
-# ANCHOR Prepare the rest of the document for conversion to json key-value pairs
+# ANCHOR Prepare the rest of the document for conversion to key-value pairs
 grep -o '^[^#]*' "$ORIG/$TICKET.md" >"$CONV/$TICKET.md"
 pandoc -o "$CONV/$TICKET-e.txt" "$CONV/$TICKET.md" --strip-comments --wrap=none
 grep -o '^[^`|^<]*' "$CONV/$TICKET-e.txt" >"$CONV/$TICKET-m.txt"
-grep "\S" "$CONV/$TICKET-m.txt" >"$CONV/$TICKET.txt"
+grep "\S" "$CONV/$TICKET-m.txt" >"$CONV/text/$TICKET.txt"
 rm "$CONV/$TICKET.md" "$CONV/$TICKET-e.txt" "$CONV/$TICKET-m.txt"
-awk 'NR>1' "$CONV/$TICKET.txt" >tmp.txt && mv tmp.txt "$CONV/$TICKET.txt"
+awk 'NR>1' "$CONV/text/$TICKET.txt" >tmp.txt && mv tmp.txt "$CONV/text/$TICKET.txt"
 
 # check line 3 for date
-awk 'NR==3' "$CONV/$TICKET.txt" >"$CONV/line_3_check.txt"
+awk 'NR==3' "$CONV/text/$TICKET.txt" >"$CONV/line_3_check.txt"
 NO_AST_CHARS=$(cut -c 5-8 "$CONV/line_3_check.txt")
 AST_CHARS=$(cut -c 7-10 "$CONV/line_3_check.txt")
 
 # Make sure date exists. If not, add it.
 if [ "$AST_CHARS" = "Vers" ]; then
     t=$(mktemp)
-    gsed '3i-   **Date:** 28 October 2021' "$CONV/$TICKET.txt" >"$t" && mv "$t" "$CONV/$TICKET.txt"
+    gsed '3i-   **Date:** 28 October 2021' "$CONV/text/$TICKET.txt" >"$t" && mv "$t" "$CONV/text/$TICKET.txt"
 elif [ "$NO_AST_CHARS" = "Vers" ]; then
     t=$(mktemp)
-    gsed '3i-   Date: 28 October 2021' "$CONV/$TICKET.txt" >"$t" && mv "$t" "$CONV/$TICKET.txt"
+    gsed '3i-   Date: 28 October 2021' "$CONV/text/$TICKET.txt" >"$t" && mv "$t" "$CONV/text/$TICKET.txt"
 fi
 
 HAS_AST="$(cut -c 5 "$CONV/line_3_check.txt")"
 rm "$CONV/line_3_check.txt"
+
 if [ "$HAS_AST" = '*' ]; then
-    head -10 "$CONV/$TICKET.txt" >>"$CONV/first-keys.txt"
-    echo "$CONV/first-keys.txt" | tr -d "*"
+    head -10 "$CONV/text/$TICKET.txt" >>"$CONV/text/first-keys.txt"
+    echo "$CONV/text/first-keys.txt" | tr -d "*"
 
     for ((i = 1; i < 11; i++)); do
-        awk "NR==$i" "$CONV/first-keys.txt" >"$CONV/$i.txt"
-        cat <"$CONV/$i.txt" | tr -d "*" >"$CONV/no-ast-keys.txt"
-        sed 's/^.\{4\}//g' "$CONV/no-ast-keys.txt" >>"$CONV/$KV.txt"
-        rm "$CONV/$i.txt"
+        awk "NR==$i" "$CONV/text/first-keys.txt" >"$CONV/text/$i.txt"
+        cat <"$CONV/text/$i.txt" | tr -d "*" >"$CONV/text/no-ast-keys.txt"
+        sed 's/^.\{4\}//g' "$CONV/text/no-ast-keys.txt" >>"$CONV/key-vals/$KV.txt"
+        rm "$CONV/text/$i.txt"
     done
-
-    rm "$CONV/first-keys.txt" "$CONV/no-ast-keys.txt"
+    rm "$CONV/text/no-ast-keys.txt"
 else
-    head -10 "$CONV/$TICKET.txt" >>"$CONV/first-keys.txt"
-    echo "$CONV/first-keys.txt" | tr -d "*"
+    head -10 "$CONV/text/$TICKET.txt" >>"$CONV/text/first-keys.txt"
+    # cat <"$CONV/text/first-keys.txt" | tr -d "*"
 
     for ((i = 1; i < 11; i++)); do
-        awk "NR==$i" "$CONV/first-keys.txt" >"$CONV/$i.txt"
-        cat <"$CONV/$i.txt" | tr -d "*" >"$CONV/ast-keys.txt"
-        sed 's/^.\{4\}//g' "$CONV/ast-keys.txt" >>"$CONV/$KV.txt"
-        rm "$CONV/$i.txt"
+        awk "NR==$i" "$CONV/text/first-keys.txt" >"$CONV/text/$i.txt"
+        cat <"$CONV/text/$i.txt" | tr -d "*" >"$CONV/text/ast-keys.txt"
+        sed 's/^.\{4\}//g' "$CONV/text/ast-keys.txt" >>"$CONV/key-vals/$KV.txt"
+        rm "$CONV/text/$i.txt"
     done
-    rm "$CONV/first-keys.txt" "$CONV/ast-keys.txt"
+    rm "$CONV/text/ast-keys.txt"
 fi
+
+rm "$CONV/text/first-keys.txt"
+
+echo "Summary: $(tail +11 "$CONV/text/$TICKET.txt")" >>"$CONV/key-vals/$KV.txt"
+mv "$CONV/key-vals/$KV.txt" "$CONV/key-vals/$TICKET.txt"
 
 # # ANCHOR create rest of keys
 # create_key "Application engineer" "AER" 1 "5-24" "7-9" "CSE"
---
+++ verify_process.sh
@@ -0,0 +1,122 @@
+#!/bin/bash
+if [ -f ./converted-resolved-tickets/shall-not-pass.txt ]; then
+    rm ./converted-resolved-tickets/shall-not-pass.txt
+fi
+
+if [ -f ./converted-resolved-tickets/pass.txt ]; then
+    rm ./converted-resolved-tickets/pass.txt
+fi
+
+if [ -f ./converted-resolved-tickets/error-log.txt ]; then
+    rm ./converted-resolved-tickets/error-log.txt
+fi
+
+if [ ! -d ./converted-resolved-tickets/failed ]; then
+    mkdir ./converted-resolved-tickets/failed
+fi
+
+if [ ! -d ./converted-resolved-tickets/passed ]; then
+    mkdir ./converted-resolved-tickets/passed
+fi
+
+PASS=0
+FAIL=0
+FAILURES=false
+log=./converted-resolved-tickets/error-log.txt
+
+get_failure_rate() {
+    not_passed="$1"
+    total="$2"
+
+    x=$((not_passed * 100))
+    y=$((x / total))
+
+    echo "DONE: tests finished with $y% failure rate"
+}
+
+is_equal_to() {
+    expected=$1
+    actual=$2
+    file=$3
+    line_num=$4
+    log=./converted-resolved-tickets/error-log.txt
+
+    if [ ! -f ./converted-resolved-tickets/shall-not-pass.txt ]; then
+        touch ./converted-resolved-tickets/shall-not-pass.txt
+    fi
+
+    if [ ! -f ./converted-resolved-tickets/pass.txt ]; then
+        touch ./converted-resolved-tickets/pass.txt
+    fi
+
+    if [ ! "$actual" = "$expected" ]; then
+        {
+            echo "ERROR:  ${file:38:42}"
+            printf "\n\tLine: %s\n\tExpected: \"%s\"\n\tActual: \"%s\"\n\n" "$line_num" "$expected" "$actual"
+        } >>$log
+
+        if ! grep -q "${file:38:42}" ./converted-resolved-tickets/shall-not-pass.txt; then
+            echo "${file:38:42}" >>./converted-resolved-tickets/shall-not-pass.txt
+            echo "FAILED: ${file:38:42}"
+        fi
+        FAILURES=true
+    else
+        printf "PASSED: %s\n" "${file:38:42}" >>$log
+        if [ "$FAILURES" = "false" ]; then
+            if ! grep -q "${file:38:42}" ./converted-resolved-tickets/pass.txt; then
+                echo "${file:38:42}" >>./converted-resolved-tickets/pass.txt
+            fi
+        fi
+    fi
+}
+
+for file in ./converted-resolved-tickets/key-vals/*; do
+    FAILURES=false
+
+    L1=$(head -1 "$file" | tail -1)
+    L2=$(head -2 "$file" | tail -1)
+    L3=$(head -3 "$file" | tail -1)
+    # L4=$(head -4 "$file" | tail -1)
+    L5=$(head -5 "$file" | tail -1)
+    L6=$(head -6 "$file" | tail -1)
+    L7=$(head -7 "$file" | tail -1)
+    L8=$(head -8 "$file" | tail -1)
+    L9=$(head -9 "$file" | tail -1)
+    L10=$(head -10 "$file" | tail -1)
+    L11=$(head -11 "$file" | tail -1)
+    L12=$(head -12 "$file" | tail -1)
+    L13=$(head -13 "$file" | tail -1)
+    L14=$(head -14 "$file" | tail -1)
+
+    is_equal_to "zendeskTicketNumber:" "${L1:0:20}" "$file" "1"
+    is_equal_to "zendeskLink:" "${L2:0:12}" "$file" "2"
+    is_equal_to "title:" "${L3:0:6}" "$file" "3"
+    # is_equal_to "Application engineer:" "${L4:0:21}" "$file" "4"
+    is_equal_to "Customer:" "${L5:0:9}" "$file" "5"
+    is_equal_to "Date:" "${L6:0:5}" "$file" "6"
+    is_equal_to "Version:" "${L7:0:8}" "$file" "7"
+    is_equal_to "Deployment:" "${L8:0:11}" "$file" "8"
+    is_equal_to "External Services:" "${L9:0:18}" "$file" "9"
+    is_equal_to "Auth Providers:" "${L10:0:15}" "$file" "10"
+    is_equal_to "Slack Links:" "${L11:0:12}" "$file" "11"
+    is_equal_to "GitHub Issue Link:" "${L12:0:18}" "$file" "12"
+    is_equal_to "Doc Update Link:" "${L13:0:16}" "$file" "13"
+    is_equal_to "Summary:" "${L14:0:8}" "$file" "14"
+
+    if [ "$FAILURES" = "true" ]; then
+        ((FAIL++))
+        cp "$file" "./converted-resolved-tickets/failed"
+    elif [ "$FAILURES" = "false" ]; then
+        ((PASS++))
+        cp "$file" "./converted-resolved-tickets/passed"
+    fi
+done
+
+{
+    printf "Pass: %s\nFailed: %s\n" "$PASS" "$FAIL"
+    get_failure_rate FAIL $((PASS + FAIL))
+    printf "\n***************** ERROR LOG *****************\n"
+    cat <$log
+} >./converted-resolved-tickets/results.txt
+
+# rm ./converted-resolved-tickets/pass.txt ./converted-resolved-tickets/shall-not-pass.txt ./converted-resolved-tickets/error-log.txt
